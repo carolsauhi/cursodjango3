b81a737d1c0f67070cd96c47ecb201b520d9e827
Carolina Sauhi Gomes <csauhi@br.ibm.com>
2021-08-10 16:36:06 -0300

    Configurando static_root

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -83,10 +83,11 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
